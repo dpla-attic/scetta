eb5995b4ed089e9af4f3dce90b3ab9ed3b94048a
Michael Della Bitta <mdellabitta@gmail.com>
2016-10-26 15:11:00 -0400

    added spark implementation

--- build.sbt
+++ build.sbt
@@ -4,6 +4,7 @@ version := "0.0.1"
 
 scalaVersion := "2.11.8"
 
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.2"
 libraryDependencies += "org.json4s" % "json4s-core_2.11" % "3.4.2"
 libraryDependencies += "org.json4s" % "json4s-jackson_2.11" % "3.4.2"
 libraryDependencies += "org.apache.jena" % "jena-tdb" % "3.1.0"
---
+++ src/main/scala/la/dp/scetta/SparkMain.scala
@@ -0,0 +1,38 @@
+package la.dp.scetta
+
+import java.io.{PrintWriter, StringWriter}
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkMain extends App with CdlMapping {
+
+  if (args.length != 3) {
+    println("Args: <MASTER> <IN> <OUT>")
+    sys.exit(-1)
+  }
+
+  val conf = new SparkConf()
+    .setAppName("CDL Mapping")
+    .setMaster(args(0))
+    .set("spark.executor.instances", "4")
+
+  val sc = new SparkContext(conf)
+
+  val inputData: RDD[(String, Array[Byte])] = sc.sequenceFile[String, Array[Byte]](args(1))
+  val outputData = inputData.map(record => (record._1, process(record._2)))
+  outputData.saveAsSequenceFile(args(2), Some(classOf[org.apache.hadoop.io.compress.DefaultCodec]))
+
+  def process(inputData: Array[Byte]): String = {
+    try {
+      map(new String(inputData, "utf8"))
+    } catch {
+      case e: Exception => {
+        val sw = new StringWriter
+        val pw = new PrintWriter(sw)
+        e.printStackTrace(pw)
+        sw.toString
+      }
+    }
+  }
+}
